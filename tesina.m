e5c92ff8a5c89360e3b1cd03192b70ef889c2731
MariaElenaContini <con.elena@hotmail.it>
2021-12-26 17:49:11 +0100

    Impostazione possibile analisi - leggere commenti

--- tesina.m
+++ tesina.m
@@ -19,16 +19,28 @@ end
 G=graph(Adj);
 plot(G)
 
-% PWC
+% CENTRALITA' PWC
+% dipende da quanto il grafo rimane connesso se elimino quel nodo
 importancePwc=pairwiseconnectivity(Adj,n);
 
 figure()
 stem(importancePwc)
 hold on
 
-% grado dei nodi
+% CENTRALITA' DI GRADO:
+% dipende dal grado del nodo, quando piu è grande il suo grado tanto più il
+% nodo è importante
 importanceDegree=degree(Adj);
 
 stem(importanceDegree,'r')
 
+% CENTRALITA' rispetto alla CLOSENESS
+% indica quanto un nodo è vicino agli altri nodi della rete
 
+% CENTRALITA' con metodo degli AUTOVETTORI
+% un nodo è tanto piu importante tanto è collegato a nodi altrettanto importanti 
+
+% CENTRALITA DI POSIZIONE
+% permette di determinare il nodo che occupa la posizione piu centrale
+% nella rete di modo che se deve mandare informazioni a tutti gli altri
+% nodi lo farà nel minor tempo possibile
